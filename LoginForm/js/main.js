6a5d98a6e181f29eca808c068a9b058aff72d2e5
ductoanoxo <toantra349>
2023-12-01 20:47:27 +0700

    css

--- LoginForm/js/main.js
+++ LoginForm/js/main.js
@@ -16,7 +16,7 @@
         })    
     })
   
-  
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -25,7 +25,7 @@
         var check = true;
 
         for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+            if(validate(input[i]) === false){
                 showValidate(input[i]);
                 check=false;
             }
@@ -42,13 +42,13 @@
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if($(input).val().trim() === ''){
                 return false;
             }
         }
