f8c5a4efd998ea2ce7c6fb5b24d4968bda9db3fd
ductoanoxo <toantra349>
2023-12-03 18:57:51 +0700

    done login

---
+++ LoginForm/js/test.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var loggedInUser = localStorage.getItem('loggedInUser');
+    if (loggedInUser) {
+        // Hiển thị tên người dùng
+        document.getElementById('userIcon').innerHTML = loggedInUser;
+    }
+};
+function directCart() {
+    location.href = "shoppingcart.html";
+}
+function login() {
+    location.href = "login.html";
+}
+function register()
+{
+    location.href="register.html";
+}
+function userinfo()
+{
+    location.href="userinfo.html";
+}
